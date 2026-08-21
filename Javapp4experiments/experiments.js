8b34a39adbdb49ac4b74c9fdb6c2b760ced1eb8f
Jumo0003 <Jumo0003@stud.ek.dk>
2026-08-21 10:44:51 +0200

    utillib oprate+har ikke lavet mine andre opg

---
+++ Javapp4experiments/experiments.js
@@ -0,0 +1,13 @@
+"use strict";
+
+const result = greeting("KLaus");
+
+function greeting(firstName) {
+  firstName = `Hello ${firstName}`;
+}
+return console.log(result);
+
+greeting("any name");
+return console.log(greeting("any name"));
+
+console.log("TEST");
