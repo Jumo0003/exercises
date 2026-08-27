353f3372f57763c2941f6e9e55d6429c892d83df
Jumo0003 <Jumo0003@stud.ek.dk>
2026-08-27 19:01:24 +0200

    filtreingoevelse

--- filter_oevelse_starter_files_table/filter_oevelse_starter_files_table/script.js
+++ filter_oevelse_starter_files_table/filter_oevelse_starter_files_table/script.js
@@ -11,18 +11,46 @@ const vehicles = [
   { type: "Løbehjul", passengers: 1, isElectric: true },
 ];
 const tbodyPointer = document.querySelector("tbody");
+const electricVehicles = vehicles.filter((vehicle) => vehicle.isElectric === true);
+const manySeats = vehicles.filter((vehicle) => vehicle.passengers > 2);
+const jonasEl = vehicles.filter((vehicle) => vehicle.isElectric && vehicle.ownedBy === "Jonas");
+const rugbrød = vehicles.filter((vehicle) => vehicle.fuel === "Rugbrød" && vehicle.passengers > 1);
+const søren = vehicles.filter((vehicle) => vehicle.fuel === vehicle.passengers > 100);
+
+const filters = {
+  isElectric: electricVehicles,
+  moreThan2: manySeats,
+  isElectricAndOwnedByJonas: jonasEl,
+  ryebreadFueled: rugbrød,
+  all: vehicles,
+  allTheSeats: søren,
+};
+
+const buttons = document.querySelectorAll(".btn");
+buttons.forEach((button) => {
+  button.addEventListener("click", (event) => {
+    const valgt = event.target.dataset.filter;
+    showTheseVehicles(filters[valgt]);
+  });
+});
+
 showTheseVehicles(vehicles);
 
 function showTheseVehicles(arr) {
+  tbodyPointer.innerHTML = "";
+  if (arr.length === 0) {
+    tbodyPointer.innerHTML = `<tr><td colspan="7">ingen resultater matcher din søgning</td></tr>`;
+    return;
+  }
   arr.forEach((each) => {
     tbodyPointer.innerHTML += `<tr>
-  <td>${each.type}</td>
-  <td>${each.fuel}</td>
+  <td>${each.type ? each.type : ""}</td>
+  <td>${each.fuel ? each.fuel : ""}</td>
   <td>${each.passengers}</td> 
-  <td>${each.stops}</td>
-  <td>${each.ownedBy}</td>
-  <td>${each.isElectric}</td>
-  <td>${each.isTandem}</td>
+  <td>${each.stops ? each.stops : ""}</td>
+  <td>${each.ownedBy ? each.ownedBy : ""}</td>
+  <td>${each.isElectric ? "X" : ""}</td>
+  <td>${each.isTandem ? "X" : ""}</td>
 </tr>`;
   });
 }
